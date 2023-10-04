24b038ca37595584a53069cb7a647674cb7d210d
lamnotfair <146945142+lamnotfair@users.noreply.github.com>
2023-10-04 15:57:58 +0400

    This is for download pip!
    For download pip!

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+title Downloading requriments...
+color 4
+cls
+pip install discord
+pip install colorama
+pip install smtplib
+pip install sys
+pip install os
+pip install webbroswer
+pip install asyncio
+pip install json
+pip install requests
+cls
+title AzeTool is starting...
+call start.bat
